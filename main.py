e9bb8be6d9b2f71eba4805365c72041e007d14bd
Soumajit Ghosh Dastider <devilsayan16@outlook.com>
2025-06-22 17:18:52 +0530

    Added contact route

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask import Flask, abort, render_template, redirect, url_for, flash,request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
@@ -11,11 +11,13 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm,RegisterForm,LogInForm,CommentForm
+import smtplib
+from email.mime.text import MIMEText
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
+current_year=date.today().strftime("%Y")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ["FLASK_KEY"]
 ckeditor = CKEditor(app)
@@ -126,7 +128,8 @@ def register():
             login_user(new_user)
             flash("Registered and Logged In","success")
             return redirect(url_for("get_all_posts"))
-    return render_template("register.html",form=register_form,logged_in=current_user.is_authenticated)
+    header_title = 'Register'
+    return render_template("register.html",form=register_form,logged_in=current_user.is_authenticated,title=header_title,footer=current_year)
 
 
 # TODO: Retrieve a user from the database based on their email. 
@@ -149,8 +152,8 @@ def login():
             else:
                 flash("User data not available, Please Register first","warning")
                 return redirect(url_for('register'))
-
-    return render_template("login.html",form=login_form,logged_in=current_user.is_authenticated)
+    header_title = 'LogIn'
+    return render_template("login.html",form=login_form,logged_in=current_user.is_authenticated,title=header_title,footer=current_year)
 
 
 @app.route('/logout')
@@ -165,12 +168,13 @@ def logout():
 def get_all_posts():
     result = db.session.execute(db.select(BlogPost))
     posts = result.scalars().all()
-    return render_template("index.html", all_posts=posts,logged_in=current_user.is_authenticated)
+    header_title = 'BlogHub'
+    return render_template("index.html", all_posts=posts,logged_in=current_user.is_authenticated,title=header_title,footer=current_year)
 
 
 # TODO: Allow logged-in users to comment on posts
 @app.route("/post/<int:post_id>",methods=["GET","POST"])
-@login_required
+
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
     comment_form=CommentForm()
@@ -185,7 +189,8 @@ def show_post(post_id):
         )
         db.session.add(new_comment)
         db.session.commit()
-    return render_template("post.html", post=requested_post,logged_in=current_user.is_authenticated,form=comment_form,user=current_user,gravatar=gravatar)
+    header_title = 'BlogHub'
+    return render_template("post.html", post=requested_post,logged_in=current_user.is_authenticated,form=comment_form,user=current_user,gravatar=gravatar,title=header_title,footer=current_year)
 
 
 # TODO: Use a decorator so only an admin user can create a new post
@@ -205,8 +210,9 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
+    header_title = 'New Post'
     is_admin = current_user.is_authenticated and current_user.id == 1
-    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated, is_edit=False, is_admin=is_admin)
+    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated, is_edit=False, is_admin=is_admin,title=header_title,footer=current_year)
 
 
 # TODO: Use a decorator so only an admin user can edit a post
@@ -229,8 +235,9 @@ def edit_post(post_id):
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
+    header_title = 'Edit'
     is_admin = current_user.is_authenticated and current_user.id == 1
-    return render_template("make-post.html", form=edit_form, is_edit=True, logged_in=current_user.is_authenticated, is_admin=is_admin)
+    return render_template("make-post.html", form=edit_form, is_edit=True, logged_in=current_user.is_authenticated, is_admin=is_admin,title=header_title,footer=current_year)
 
 
 # TODO: Use a decorator so only an admin user can delete a post
@@ -245,12 +252,51 @@ def delete_post(post_id):
 
 @app.route("/about")
 def about():
-    return render_template("about.html",logged_in=current_user.is_authenticated)
+    header_title = 'About'
+    return render_template("about.html",logged_in=current_user.is_authenticated,title=header_title,footer=current_year)
 
 
-@app.route("/contact")
+@app.route("/contact",methods=['GET','POST'])
 def contact():
-    return render_template("contact.html",logged_in=current_user.is_authenticated)
+    if request.method=='POST':
+        _email=os.environ["EMAIL"]
+        password=os.environ["PASS"]
+        name=request.form['name']
+        email=request.form['email']
+        ph_no=request.form['phone']
+        msg=request.form['message']
+        new_letter=f"Name: {name}\nEmail Id: {email}\nPhone no: {ph_no}\nMessage:{msg}"
+        data=[name,email,ph_no,msg]
+
+        email_message = MIMEText(new_letter)
+        email_message['Subject'] = f"Feedback from {name}"
+        email_message['From'] = _email
+        email_message['To'] = "devilsayan16@gmail.com"
+
+        try:
+            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+                connection.starttls()
+                connection.login(user=_email, password=password)
+                connection.sendmail(from_addr=_email, to_addrs=["devilsayan16@gmail.com"],
+                                    msg=email_message.as_string())
+            #print(f"Email sent successfully to: {"devilsayan16@gmail.com"}")  # Detailed success message
+            flash("Successfully Sent Your Message","success")
+
+        except smtplib.SMTPAuthenticationError:
+            print("Authentication error. Check your email and password (or App Password).")
+            flash("Something Went Wrong","danger")
+        except smtplib.SMTPDataError as e:
+            print(f"SMTP Data Error: {e}")
+            flash("Something Went Wrong","danger")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            flash("Something Went Wrong","danger")
+        #print("Email sending process completed for this birthday.")  # End of email process
+        return render_template("contact.html",msg_sent=True)
+    else:
+        header_title = 'Contact'
+        return render_template("contact.html",title=header_title,logged_in=current_user.is_authenticated,footer=current_year)
+    
 
 
 if __name__ == "__main__":
